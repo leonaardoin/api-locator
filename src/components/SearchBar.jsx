e901afc215a623aba9024252cfd44ddbdd43a4be
Léona Ardoin <126014224+leonaardoin@users.noreply.github.com>
2023-05-02 17:45:08 -0400

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,15 +6,16 @@ const SearchBar = ({
     isLoading,
     setQuery
 }) => {
+    //component renders search bar form for user query input and a submit button to search
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}> //function: handles submission of form
             <input 
-                value={query}
+                value={query} //string: current value of input field
                 className="form-control"
                 placeholder="search keywords"
                 name="query"
-                disabled={isLoading}
-                onChange={(event) => setQuery(event.target.value)}
+                disabled={isLoading} //boolean: indicates whether search is loading or not
+                onChange={(event) => setQuery(event.target.value)} //function: sets value of input field
             />   
             <input
                 disabled={isLoading || !query}
